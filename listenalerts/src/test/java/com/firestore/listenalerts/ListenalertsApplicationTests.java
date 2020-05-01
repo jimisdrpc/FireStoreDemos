5e51ec62e0037960fe42b073ee42528e52016e90
Jimis.drpc <jimis.drpc@gmail.com>
2020-05-01 20:38:21 -0300

    created Spring Boot subscribing to Google Cloud Pub/Sub for being notifed from Firestore Alerts

---
+++ listenalerts/src/test/java/com/firestore/listenalerts/ListenalertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firestore.listenalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenalertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
