645dec3b8c08c5b655cac75c64787350f616f632
Jimis.drpc <jimis.drpc@gmail.com>
2020-05-05 12:22:21 -0300

    added custonToken during listening

--- angular-firestore/src/app/app.component.ts
+++ angular-firestore/src/app/app.component.ts
@@ -6,74 +6,37 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import 'rxjs/Rx';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   public transfers: Observable<any[]>;
-  uniqueTransfer: any;
 
   transferCollectionRef: AngularFirestoreCollection<any>;
 
-  constructor(db: AngularFirestore) {
+  constructor(public auth: AngularFireAuth, public db: AngularFirestore) {
+    this.listenSingleTransferWithToken();
+  }
 
-    this.transferCollectionRef = db.collection<any>('transfer', ref => ref.where("id", "==", "1"));
+  async listenAllTransfersWithToken() {
+    await this.auth.signInWithCustomToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTU4ODY4ODk0MSwiZXhwIjoxNTg4NjkyNTQxLCJpc3MiOiJmaXJldGVzdGppbWlzQGFwcHNwb3QuZ3NlcnZpY2VhY2NvdW50LmNvbSIsInN1YiI6ImZpcmV0ZXN0amltaXNAYXBwc3BvdC5nc2VydmljZWFjY291bnQuY29tIiwidWlkIjoiTlNCRnUyWUpORGdMUUpDWjk5ZFJKbFA0RFJvMiJ9.D3L2AUHGFVAqrcSRMy_3v___iNL4v9TGnvNEksbb16CAZ-35We7bb6seJTKIEm5qgunXpf_u1LdbA4GKyaRksQp9Df8zqZiO_4zK-NJqNo_qFguCAXPcmQwmS07DF1J4CfMDVMGAsnuajSVTTL7PJpQ30F0SpZ0fqcEOix2Y_k1wk5ladhzBsGfUjM2CpdaAFZzoHbQSbTNTCFITs1NTKNt8vOxG6vxG5nSlHSTdDirY3zOx0qDuc_HsrZxt8x-fZiB0Nme8-WQseY87Go6y_5IccxNTc2qcErQ43z4NjkLdbRRKLcT7dQyAKTtBOPAPa0irOELo5usmHw8l-NO-rw");
+    this.transfers = this.db.collection('/transfer').valueChanges();
+  }
+
+  async listenSingleTransferWithToken() {
+    await this.auth.signInWithCustomToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTU4ODY4ODk0MSwiZXhwIjoxNTg4NjkyNTQxLCJpc3MiOiJmaXJldGVzdGppbWlzQGFwcHNwb3QuZ3NlcnZpY2VhY2NvdW50LmNvbSIsInN1YiI6ImZpcmV0ZXN0amltaXNAYXBwc3BvdC5nc2VydmljZWFjY291bnQuY29tIiwidWlkIjoiTlNCRnUyWUpORGdMUUpDWjk5ZFJKbFA0RFJvMiJ9.D3L2AUHGFVAqrcSRMy_3v___iNL4v9TGnvNEksbb16CAZ-35We7bb6seJTKIEm5qgunXpf_u1LdbA4GKyaRksQp9Df8zqZiO_4zK-NJqNo_qFguCAXPcmQwmS07DF1J4CfMDVMGAsnuajSVTTL7PJpQ30F0SpZ0fqcEOix2Y_k1wk5ladhzBsGfUjM2CpdaAFZzoHbQSbTNTCFITs1NTKNt8vOxG6vxG5nSlHSTdDirY3zOx0qDuc_HsrZxt8x-fZiB0Nme8-WQseY87Go6y_5IccxNTc2qcErQ43z4NjkLdbRRKLcT7dQyAKTtBOPAPa0irOELo5usmHw8l-NO-rw");
+    this.transferCollectionRef = this.db.collection<any>('transfer', ref => ref.where("id", "==", "1"));
     this.transfers = this.transferCollectionRef.snapshotChanges().map(actions => {
       return actions.map(action => {
-        const data = action.payload.doc.data();// as Todo;
+        const data = action.payload.doc.data();
         const id = action.payload.doc.id;
         return { id, ...data };
       });
     });
 
-    //this.transfers = db.collection('/transfer').valueChanges(); 
-    //this.transfers = db.doc<any>(`transfer/sDme6IRIi4ezfeyfrU7y`).valueChanges();
-
-    // this.uniqueTransfer = db.collection('/transfer',
-    //   ref => ref.where("id", "==", "1"))
-    //   .snapshotChanges().pipe(map(actions => actions.map(a => a.payload.doc.data()))
-    //   );;
-
-    // this.uniqueTransfer = db.collection('/transfer', ref => ref.where("id", "==", "1")).snapshotChanges().pipe(
-    //   map(actions => actions.map(a => {
-    //     const data = a.payload.doc.data();
-    //     return { data };
-    //   }))
-
-
-    // db.doc(`transfer/Wsj0dysyHHok3xupwDhD`)
-    //   .snapshotChanges()
-    //   .pipe()
-    //   .subscribe();
-
   }
 
-
-  /*
-    constructor(private http: HttpClient) {
-      this.getTranfers();
-    }
-    
-    public getTranfers() {
-  
-      const headers = { 'Authorization': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjBiYWJiMjI0NDBkYTAzMmM1ZDAwNDJjZGFhOWQyODVjZjhkMjAyYzQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZmlyZXRlc3RqaW1pcyIsImF1ZCI6ImZpcmV0ZXN0amltaXMiLCJhdXRoX3RpbWUiOjE1ODc1MDQ4NTcsInVzZXJfaWQiOiJOU0JGdTJZSk5EZ0xRSkNaOTlkUkpsUDREUm8yIiwic3ViIjoiTlNCRnUyWUpORGdMUUpDWjk5ZFJKbFA0RFJvMiIsImlhdCI6MTU4NzUwNDg1NywiZXhwIjoxNTg3NTA4NDU3LCJlbWFpbCI6ImppbWlzLmRycGNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImppbWlzLmRycGNAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiY3VzdG9tIn19.NHIonghH8alr9DUMDD7ddudVfIefxZ9E_m4N49ig-WPmX_qxwJQFlzYp2En3klpCSXJ3uJvS5lkBWBLjdPF3TRc9hkyCTd_Fkpg5syobv14QMYNCZ_d3Abymghe2aiW4jA6koglO93dBJYkk-6RYcRf68dBBkKX9_Qnf5qN9tFQZW_kfBxxokQ6H1s_YZstbaizr8QSrVg10erteD-8MXwYAtDeA29Nipfw8KVuYdJwqeUN9iWuerbpx4-6Uion1qy-bOD5WVgRDkmkeZu01DxebKoUmuzLxYQ1j4Q1ndMH6qoY83ab21FkUifgEtHT4ZXde1ehf_PIwNDx6_ZfJuQ' }
-      const body = JSON.stringify({
-        "structuredQuery": {
-          "where": {
-            "fieldFilter": {
-              "field": { "fieldPath": "id" },
-              "op": "EQUAL",
-              "value": { "stringValue": "4" }
-            }
-          },
-          "from": [{ "collectionId": "transfer" }]
-        }
-      })
-  
-      this.http.post<any>('https://firestore.googleapis.com/v1/projects/firetestjimis/databases/(default)/documents:runQuery', body, { headers }).subscribe(data => {
-        this.transfers = data;
-      })
-    }
-    */
 }
