df78bc2d1a86fd574996e0fff49f6ce6eb1552e0
Jimis.drpc <jimis.drpc@gmail.com>
2020-06-01 16:13:32 -0300

    snapshot working

--- AppFirestore/app/src/main/java/com/mycomp/appfirestore/MainActivity.kt
+++ AppFirestore/app/src/main/java/com/mycomp/appfirestore/MainActivity.kt
@@ -1,29 +1,34 @@
 package com.mycomp.appfirestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mycomp.appfirestore.data.service.Endpoint
 import com.mycomp.appfirestore.data.service.NetworkUtils
-import com.mycomp.appfirestore.model.Posts
 import com.mycomp.appfirestore.model.Transfer
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnTransfer=findViewById(R.id.btnTransfer) as Button
-        val textViewTransfer=findViewById(R.id.textViewTransfer) as TextView
+        val btnTransfer = findViewById(R.id.btnTransfer) as Button
+        val textViewTransfer = findViewById(R.id.textViewTransfer) as TextView
         btnTransfer.setOnClickListener {
             postTransfer()
         }
@@ -35,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
         val endpoint = retrofitClient.create(Endpoint::class.java)
 
-        val transfer = Transfer("Demetrio","Jimis","100","","")
+        val transfer = Transfer("Demetrio", "Jimis", "100", "", "")
         val callback = endpoint.postTransfer(transfer)
 
         callback.enqueue(object : Callback<Transfer> {
@@ -44,66 +49,82 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: Call<Transfer>, response: Response<Transfer>) {
-                textViewTransfer.text = response.body().toString()
+                //textViewTransfer.text = response.body().toString()
+                listenStatus()
             }
         })
 
     }
 
+    fun listenStatus() {
+        val TAG = "ListenStatus"
+        auth = FirebaseAuth.getInstance()
 
-//    fun postTransfer() {
-//        val client = OkHttpClient()
-//        val url = URL("http://localhost:8080")
-//
-//        //just a string
-//        //var jsonString = "{\"name\": \"Rolando\", \"job\": \"Fakeador\"}"
-//
-//        //or using jackson
-//        val mapperAll = ObjectMapper()
-//        val jacksonObj = mapperAll.createObjectNode()
-//        jacksonObj.put("name", "Rolando")
-//        jacksonObj.put("job", "Fakeador")
-//        val jacksonString = jacksonObj.toString()
-//
-//        val mediaType = "application/json; charset=utf-8".toMediaType()
-//        val body = jacksonString.toRequestBody(mediaType)
-//
-//        val request = Request.Builder()
-//            .url(url)
-//            .post(body)
-//            .build()
-//
-//        val response = client.newCall(request).execute()
-//
-//        val responseBody = response.body!!.string()
-//
-//        //Response
-//        println("Response Body: " + responseBody)
-//
-//        //we could use jackson if we got a JSON
-//        val objData = mapperAll.readTree(responseBody)
-//
-//        println("My name is " + objData.get("name").textValue() + ", and I'm a " + objData.get("job").textValue() + ".")
-//    }
+        auth.signInWithCustomToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTU5MTAzNzUzNSwiZXhwIjoxNTkxMDQxMTM1LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1mZzZwOUBmaXJldGVzdGppbWlzLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstZmc2cDlAZmlyZXRlc3RqaW1pcy5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6Ik5TQkZ1MllKTkRnTFFKQ1o5OWRSSmxQNERSbzIifQ.wfcnWlA5DvFUBRfVWjNizxRWQ3VygPOcD_gl6ijF10c0uw3fLzL08iSo3DAzrazPrE1cqRfmlQvR1duhaNiSOoZvPyvR1rdk4Nsvc5F9r4x7YVpBrusD79wOlPbS7eSTr5rX2n6Wm9jd6kmUr_ThKpoMTUcb4HFul442SlRwKIANDEmeB0JDtpXh4G8cQ5vdIAJ5OWEWKrFmdYakLUSveyifdskN3ozxb6b4E2dVIMn7Fest1XE3HtzbGIrkqScihmYURAS06b2xtyRf-EEwbB8tfsbe-Sj3NiGanzi0ZIz7wyyo0RCBunVbU6zRpDBjEgf5KlEZZZyhD6J39BgXsQ")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "*** signInWithCustomToken:success")
+                    startSnapshot()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
 
-//    fun getData() {
-//        val retrofitClient = NetworkUtils
-//            .getRetrofitInstance("https://jsonplaceholder.typicode.com")
-//
-//        val endpoint = retrofitClient.create(Endpoint::class.java)
-//        val callback = endpoint.getPosts()
-//
-//        callback.enqueue(object : Callback<List<Posts>> {
-//            override fun onFailure(call: Call<List<Posts>>, t: Throwable) {
-//                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
-//            }
-//
-//            override fun onResponse(call: Call<List<Posts>>, response: Response<List<Posts>>) {
-//                response.body()?.forEach {
-//                    textView.text = textView.text.toString().plus(it.body)
+
+    fun startSnapshot() {
+        val TAG = "StartSnapshot"
+
+        //Try to pass this(activity context) as first parameter.It will automatically handle acivity life cycle.
+        // Example if you are calling this listener in onCreate() and passing this as a first parameter then
+        // it will remove this listener in onDestroy() method of activity.
+        val listenerReg = FirebaseFirestore.getInstance().collection("transfer")
+            .document("sDme6IRIi4ezfeyfrU7y")
+            .addSnapshotListener(
+                this,
+                EventListener<DocumentSnapshot?> { snapshot, e ->
+                    if (e != null) {
+                        Log.w(TAG, "Listen failed.", e)
+                        return@EventListener
+                    }
+                    if (snapshot != null && snapshot.exists()) {
+                        textViewTransfer.text = snapshot.data!!["status"].toString()
+                        //Log.d(TAG, snapshot.data!!["status"].toString())
+                    } else {
+                        Log.d(TAG, "Current data: null")
+                    }
+                })
+
+
+        //search accroos all collection
+//        FirebaseFirestore.getInstance().collection("transfer")
+//            .whereEqualTo("id", "1")
+//            .addSnapshotListener { value, e ->
+//                if (e != null) {
+//                    Log.w(TAG, "Listen failed.", e)
+//                    return@addSnapshotListener
 //                }
-//            }
-//        })
 //
+//                val transfer = ArrayList<String>()
+//                for (doc in value!!) {
+//                    doc.getString("status")?.let {
+//                        transfer.add(it)
+//                    }
+//                }
+//
+//                Log.d(TAG, "*** transfer: $transfer")
+//            }
+    }
+
+//    override fun onStop() {
+//        super.onStop()
+//        if (listenerReg != null) {
+//            listenerReg.remove()
+//        }
 //    }
 }
--- AppFirestore/app/src/main/java/com/mycomp/appfirestore/data/service/Endpoint.kt
+++ AppFirestore/app/src/main/java/com/mycomp/appfirestore/data/service/Endpoint.kt
@@ -1,15 +1,11 @@
 package com.mycomp.appfirestore.data.service
 
-import com.mycomp.appfirestore.model.Posts
 import com.mycomp.appfirestore.model.Transfer
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface Endpoint {
-    @GET("posts")
-    fun getPosts() : Call<List<Posts>>
 
     @POST("/transfers")
     fun postTransfer(@Body transfer: Transfer): Call<Transfer>
